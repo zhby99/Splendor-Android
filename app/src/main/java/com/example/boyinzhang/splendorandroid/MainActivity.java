9566bcd6a942eca610415da58b359393f0c67f51
zhby99 <bzhang70@illinois.edu>
2017-05-26 16:42:18 +0800

    add button listeners

--- app/src/main/java/com/example/boyinzhang/splendorandroid/MainActivity.java
+++ app/src/main/java/com/example/boyinzhang/splendorandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.boyinzhang.splendorandroid;
 
+import android.app.AlertDialog;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -13,17 +14,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.boyinzhang.splendorandroid.Game.Game;
+import com.example.boyinzhang.splendorandroid.Model.Card;
+import com.example.boyinzhang.splendorandroid.Model.utils.GemInfo;
 
 import static com.example.boyinzhang.splendorandroid.View.ImageUtil.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    Game game;
+    GemInfo currentGemInfo;
+    Card selectedCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addGemButtonListeners();
+
 
         ImageView noble1 = (ImageView) findViewById(R.id.noble1);
         Bitmap src = drawableToBitamp(getResources().getDrawable(R.drawable.female_noble));
@@ -52,9 +62,86 @@ public class MainActivity extends AppCompatActivity {
         noble1.setImageDrawable(newDrawable);
 
 
+        this.game = new Game();
+        currentGemInfo = new GemInfo(0);
+        addGemButtonListeners();
+        addButtonListeners();
 
 
 
+
+
+
+    }
+
+    private void addButtonListeners() {
+        Button resetButton = (Button) findViewById(R.id.reset);
+        Button collectButton = (Button) findViewById(R.id.collect);
+        Button buyButton = (Button) findViewById(R.id.buy);
+        Button reserveButton = (Button) findViewById(R.id.reserve);
+        resetButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentGemInfo.reset();
+                LinearLayout gemBoard = (LinearLayout) findViewById(R.id.gem_board);
+                RelativeLayout gemBoardChild = (RelativeLayout) gemBoard.getChildAt(0);
+                for(int i =1; i<5; i++){
+                    RelativeLayout tempChild = (RelativeLayout) gemBoardChild.getChildAt(i);
+                    TextView tempView = (TextView) tempChild.getChildAt(1);
+                    tempView.setText("0");
+                }
+            }
+        });
+        collectButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!game.getCurrentPlayer().collectGems(currentGemInfo)){
+                    Toast.makeText(MainActivity.this, "You cannot make this collection! Please try again!", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    currentGemInfo.reset();
+                    game.turnToNextPlayer();
+                }
+            }
+        });
+
+        buyButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!game.getCurrentPlayer().buyCard(selectedCard, selectedCard.isReserved())){
+                    Toast.makeText(MainActivity.this, "You cannot make this purchase! Please try again!", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Card newCard = game.getGameBoard().getNewCard(selectedCard.getPosition()[0]);
+                    game.getGameBoard().setCardOnBoard(newCard, selectedCard.getPosition());
+                    selectedCard = null;
+                    game.getCurrentPlayer().recruitAvailableNobles();
+                    if (game.currentPlayer.hasWon()) {
+                        game.turnToNextPlayer();
+                        new  AlertDialog.Builder(getApplicationContext())
+                                .setTitle("Game End" )
+                                .setMessage("You have won! Congrats!" )
+                                .setPositiveButton("Yes" ,  null )
+                                .show();
+                    }
+                }
+            }
+        });
+
+        reserveButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!game.getCurrentPlayer().reserveCard(selectedCard)){
+                    Toast.makeText(MainActivity.this, "You cannot reserve this card! Please try again!", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Card newCard = game.getGameBoard().getNewCard(selectedCard.getPosition()[0]);
+                    game.getGameBoard().setCardOnBoard(newCard, selectedCard.getPosition());
+                    selectedCard = null;
+                    game.turnToNextPlayer();
+                }
+            }
+        });
     }
 
     private void addGemButtonListeners() {
@@ -63,9 +150,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 TextView diamondNumView = (TextView) findViewById(R.id.diamond_num);
-                int currentNum = Integer.parseInt((String) diamondNumView.getText());
-                currentNum++;
-                diamondNumView.setText(Integer.toString(currentNum));
+                currentGemInfo.updateInfo(1,0,0,0,0);
+                diamondNumView.setText(Integer.toString(currentGemInfo.diamond));
             }
         });
 
@@ -74,9 +160,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 TextView emeraldNumView = (TextView) findViewById(R.id.emerald_num);
-                int currentNum = Integer.parseInt((String) emeraldNumView.getText());
-                currentNum++;
-                emeraldNumView.setText(Integer.toString(currentNum));
+                currentGemInfo.updateInfo(0,1,0,0,0);
+                emeraldNumView.setText(Integer.toString(currentGemInfo.emerald));
             }
         });
 
@@ -85,9 +170,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 TextView onyxNumView = (TextView) findViewById(R.id.onyx_num);
-                int currentNum = Integer.parseInt((String) onyxNumView.getText());
-                currentNum++;
-                onyxNumView.setText(Integer.toString(currentNum));
+                currentGemInfo.updateInfo(0,0,1,0,0);
+                onyxNumView.setText(Integer.toString(currentGemInfo.onyx));
             }
         });
 
@@ -96,9 +180,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 TextView rubyNumView = (TextView) findViewById(R.id.ruby_num);
-                int currentNum = Integer.parseInt((String) rubyNumView.getText());
-                currentNum++;
-                rubyNumView.setText(Integer.toString(currentNum));
+                currentGemInfo.updateInfo(0,0,0,1,0);
+                rubyNumView.setText(Integer.toString(currentGemInfo.ruby));
             }
         });
 
@@ -107,9 +190,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 TextView sapphireNumView = (TextView) findViewById(R.id.sapphire_num);
-                int currentNum = Integer.parseInt((String) sapphireNumView.getText());
-                currentNum++;
-                sapphireNumView.setText(Integer.toString(currentNum));
+                currentGemInfo.updateInfo(0,0,0,0,1);
+                sapphireNumView.setText(Integer.toString(currentGemInfo.sapphire));
             }
         });
     }
